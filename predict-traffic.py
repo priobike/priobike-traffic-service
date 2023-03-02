65c37f07025aa99fbfa6a8d4e17180ed1f5ba405
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-03-02 10:54:20 +0100

    save prediction to json

--- predict-traffic.py
+++ predict-traffic.py
@@ -6,16 +6,13 @@ time_now = int(time.time())
 time_7_days_ago = time_now - 60 * 60 * 24 * 7
 
 
-def build_average(hour_now, files):
+def calculate_historic_average(hour_now, files):
     """
     Build average for a given hour over max. last 7 days
     Returns the average score for the given hour or None if no data is available
     """
 
     global time_now, time_7_days_ago
-
-    # print(time_now)
-    # print(time_7_days_ago)
 
     scores = []
 
@@ -36,9 +33,8 @@ def build_average(hour_now, files):
                 data = json.load(file)
                 score = data["trafficflow"]
                 scores.append(score)
-                print(scores)
 
-    # calculate average score
+    # Calculate average score
     if len(scores) != 0:
         return sum(scores) / len(scores)
     else:
@@ -46,24 +42,32 @@ def build_average(hour_now, files):
 
 
 def main():
-
-    # checks files in the history folder
+    # Get files in the history folder
     files = os.listdir("history")
     files = [file for file in files if file.endswith(".json")]
     files.sort(reverse=True)
 
     hour_now = int(time.strftime("%H", time.localtime()))
-    day_now = int(time.strftime("%d", time.localtime()))
-    month_now = int(time.strftime("%m", time.localtime()))
-    year_now = int(time.strftime("%Y", time.localtime()))
-    print(hour_now, day_now, month_now, year_now)
 
-    # get the average scores from hour -1 to hour +3
-    hour_score = build_average(hour_now, files)
-    next_hour_score = build_average(hour_now + 1, files)
-    next2_hour_score = build_average(hour_now + 2, files)
-    next3_hour_score = build_average(hour_now + 3, files)
-    prev_hour_score = build_average(hour_now - 1, files)
+    prediction = {}
+
+    # Get the average scores from hour -1 to hour +4
+    for offset in range(-1, 4 + 1, 1):
+        hour_score = calculate_historic_average(hour_now + offset, files)
+        prediction.update({hour_now + offset: hour_score})
+
+        print("Hour:", hour_now + offset, "\tScore:", hour_score)
+
+    # Save prediction to JSON file
+    if not os.path.exists("prediction"):
+        os.makedirs("prediction")
+
+    date_output = time.strftime("%d.%m.%Y", time.localtime())
+    hour_output = time.strftime("%H:%M", time.localtime())
+
+    # save data to json file
+    with open(f"prediction/{date_output}-{hour_output}.json", "w") as outfile:
+        json.dump(prediction, outfile, indent=4)
 
 
 if __name__ == "__main__":
