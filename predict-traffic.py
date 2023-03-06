c3cd9da04ac8047e920dddf4655d8ad1676a729b
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-03-06 10:22:57 +0100

    prefere scores from same day or weekday/weekend

--- predict-traffic.py
+++ predict-traffic.py
@@ -4,15 +4,20 @@ import time
 
 time_now = int(time.time())
 time_60_days_ago = time_now - 60 * 60 * 24 * 60
+date_now = time.strftime("%d.%m.%Y", time.localtime())
+weekday_now = int(time.strftime("%w", time.localtime()))
 
 
-def calculate_historic_average(hour_now, history_dir, files):
+def calculate_historic_average(check_hour, history_dir, files):
     """
-    Build average for a given hour over max. last 60 days
-    Returns the average score for the given hour or None if no data is available
+    Build average for a given hour over max. last 60 days and deletes older files.
+    It first tries to get the score for the given hour and same day of the week.
+    If there is not enough data, it tries to get the score for the given hour within workweek/weekend.
+    If there is still not enough data, it tries to get the score for the given hour.
+    Returns the average score or None if no data is available.
     """
 
-    global time_now, time_60_days_ago
+    global time_now, time_60_days_ago, date_now, weekday_now
 
     scores = []
 
@@ -25,11 +30,41 @@ def calculate_historic_average(hour_now, history_dir, files):
             os.remove(f"{history_dir or 'history'}/{filename}")
             continue
 
-        # extract hour from timestamp
+        # extract time from timestamp
+        data_day = int(time.strftime("%w", time.localtime(timestamp)))
         data_hour = int(time.strftime("%H", time.localtime(timestamp)))
+        data_date = time.strftime("%d.%m.%Y", time.localtime(timestamp))
 
-        if data_hour == hour_now:
-            # open json file and get score
+        # If we have data for the given hour and same day of the week
+        if data_hour == check_hour and data_day == weekday_now and data_date != date_now:
+            with open(f"{history_dir or 'history'}/{filename}", "r") as file:
+                data = json.load(file)
+                score = data["trafficflow"]
+                scores.append(score)
+            continue
+
+        # If there is not enough data, get the average score for the given hour on workweek/weekend
+        if weekday_now <= 5:
+            # workweek
+            if data_hour == check_hour and data_day <= 5:
+                with open(f"{history_dir or 'history'}/{filename}",
+                          "r") as file:
+                    data = json.load(file)
+                    score = data["trafficflow"]
+                    scores.append(score)
+                continue
+        else:
+            # weekend
+            if data_hour == check_hour and data_day > 5:
+                with open(f"{history_dir or 'history'}/{filename}",
+                          "r") as file:
+                    data = json.load(file)
+                    score = data["trafficflow"]
+                    scores.append(score)
+                continue
+
+        # If there is not enough data, get the average score for the given hour
+        if data_hour == check_hour:
             with open(f"{history_dir or 'history'}/{filename}", "r") as file:
                 data = json.load(file)
                 score = data["trafficflow"]
@@ -54,7 +89,6 @@ def main(history_dir, prediction_path):
     files.sort(key=key, reverse=True)
 
     hour_now = int(time.strftime("%H", time.localtime()))
-
     prediction = {}
 
     # Get the average scores from hour -1 to hour +5
