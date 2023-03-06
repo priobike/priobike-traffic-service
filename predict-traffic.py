8be11caabf25ac759bd51ea67e11c391b463f6a0
Charles Krüger <charles.krueger@mailbox.tu-dresden.de>
2023-03-06 11:15:03 +0100

    format main method on top

--- predict-traffic.py
+++ predict-traffic.py
@@ -10,6 +10,56 @@ date_now = time.strftime("%d.%m.%Y", time.localtime())
 weekday_now = int(time.strftime("%w", time.localtime()))
 
 
+def main(history_dir, prediction_path):
+    """
+    Read history json files and calculate average score for each hour to make a prediction
+    Build average for a given hour over max. last 60 days (see "days_to_include").
+    It first tries to get the score for the given hour and same day of the week (marked in prediction.json as HIGH quality)
+    If there is not enough data, it tries to get the score for the given hour within the whole workweek/weekend (marked in prediction.json as MEDIUM quality)
+    If there is still not enough data, it tries to get the score for the given hour, no matter the day (marked in prediction.json as LOW quality)
+    """
+
+    # Get files in the history folder
+    files = os.listdir(history_dir or "history")
+    files = [file for file in files if file.endswith(".json")]
+    key = lambda x: int(time.mktime(time.strptime(x, "%d.%m.%Y-%H:%M.json")))
+    files.sort(key=key, reverse=True)
+
+    prune_old_files(history_dir, files)
+
+    hour_now = int(time.strftime("%H", time.localtime()))
+    prediction = {}
+
+    # Get the average scores from hour now-1 to hour now+5
+    for offset in range(-1, 5 + 1, 1):
+        prediction_data = use_same_day(hour_now + offset, history_dir, files)
+        if prediction_data is not None:
+            prediction.update({hour_now + offset: prediction_data})
+            prediction.update({"QUALITY " + str(hour_now + offset): "HIGH"})
+            continue
+        prediction_data = use_weekday_or_weekend(hour_now + offset,
+                                                 history_dir, files)
+        if prediction_data is not None:
+            prediction.update({hour_now + offset: prediction_data})
+            prediction.update({"QUALITY " + str(hour_now + offset): "MEDIUM"})
+            continue
+        prediction_data = use_same_hour(hour_now + offset, history_dir, files)
+        if prediction_data is not None:
+            prediction.update({hour_now + offset: prediction_data})
+            prediction.update({"QUALITY " + str(hour_now + offset): "LOW"})
+            continue
+        prediction.update({hour_now + offset: None})
+
+    # Get the current score by reading the first file (which is the newest one, because the list is sorted)
+    with open(f"{history_dir or 'history'}/{files[0]}", "r") as file:
+        data = json.load(file)
+        score_now = data["trafficflow"]
+        prediction.update({"now": score_now})
+
+    with open(prediction_path or "prediction.json", "w") as outfile:
+        json.dump(prediction, outfile, indent=4)
+
+
 def prune_old_files(history_dir, files):
     """
     Deletes files older than 60 days (see "days_to_include") and removes them from the list.
@@ -118,56 +168,6 @@ def use_same_hour(check_hour, history_dir, files):
 
     # Return average score
     return sum(scores) / len(scores) if len(scores) != 0 else None
-
-
-def main(history_dir, prediction_path):
-    """
-    Read history json files and calculate average score for each hour to make a prediction
-    Build average for a given hour over max. last 60 days (see "days_to_include").
-    It first tries to get the score for the given hour and same day of the week (marked in prediction.json as HIGH quality)
-    If there is not enough data, it tries to get the score for the given hour within the whole workweek/weekend (marked in prediction.json as MEDIUM quality)
-    If there is still not enough data, it tries to get the score for the given hour, no matter the day (marked in prediction.json as LOW quality)
-    """
-
-    # Get files in the history folder
-    files = os.listdir(history_dir or "history")
-    files = [file for file in files if file.endswith(".json")]
-    key = lambda x: int(time.mktime(time.strptime(x, "%d.%m.%Y-%H:%M.json")))
-    files.sort(key=key, reverse=True)
-
-    prune_old_files(history_dir, files)
-
-    hour_now = int(time.strftime("%H", time.localtime()))
-    prediction = {}
-
-    # Get the average scores from hour now-1 to hour now+5
-    for offset in range(-1, 5 + 1, 1):
-        prediction_data = use_same_day(hour_now + offset, history_dir, files)
-        if prediction_data is not None:
-            prediction.update({hour_now + offset: prediction_data})
-            prediction.update({"QUALITY " + str(hour_now + offset): "HIGH"})
-            continue
-        prediction_data = use_weekday_or_weekend(hour_now + offset,
-                                                 history_dir, files)
-        if prediction_data is not None:
-            prediction.update({hour_now + offset: prediction_data})
-            prediction.update({"QUALITY " + str(hour_now + offset): "MEDIUM"})
-            continue
-        prediction_data = use_same_hour(hour_now + offset, history_dir, files)
-        if prediction_data is not None:
-            prediction.update({hour_now + offset: prediction_data})
-            prediction.update({"QUALITY " + str(hour_now + offset): "LOW"})
-            continue
-        prediction.update({hour_now + offset: None})
-
-    # Get the current score by reading the first file (which is the newest one, because the list is sorted)
-    with open(f"{history_dir or 'history'}/{files[0]}", "r") as file:
-        data = json.load(file)
-        score_now = data["trafficflow"]
-        prediction.update({"now": score_now})
-
-    with open(prediction_path or "prediction.json", "w") as outfile:
-        json.dump(prediction, outfile, indent=4)
 
 
 if __name__ == "__main__":
